f773f8ad8240d1d47a82da6cf739b3a099f32113
wichru <35730896+wichru@users.noreply.github.com>
2018-01-31 13:13:39 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tekst");
+            Console.WriteLine("Zapisany");
+            Console.WriteLine("W 3 liniach");
+            Console.ReadKey();
+        }
+    }
+}
